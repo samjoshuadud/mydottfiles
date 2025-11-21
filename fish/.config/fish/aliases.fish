5a32391ce3079af7c9f5bc1d9a8a698eec4751b1
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2025-11-21 13:15:54 +0800

    trash-cli

--- fish/.config/fish/aliases.fish
+++ fish/.config/fish/aliases.fish
@@ -4,5 +4,6 @@ alias kittyfetch 'clear && neofetch --config ~/.config/neofetch/kitty.conf'
 alias sansfetch 'clear && neofetch --config ~/.config/neofetch/adrien.conf --kitty '~/.config/neofetch/sans.png' --size 270px --colors 1 7 7 1 7 7'
 alias adrienfetch 'clear && neofetch --config ~/.config/neofetch/adrien.conf --kitty '~/.config/neofetch/adrien.png' --size 270px --colors 1 7 7 1 7 7'
 
+alias rm 'trash-put'
 # Cava gradient support for Tmux
 alias cavax 'TERM=st-256color cava'
