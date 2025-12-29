6778233bb7e96670683c00ac14ebc785876e5978
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2025-12-30 00:54:30 +0800

    transparent bg

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -9,15 +9,15 @@ return {
     config = function()
       require("tokyonight").setup({
         style = "night", -- storm, moon, or night
-        transparent = false,
+        transparent = true,
         terminal_colors = true,
         styles = {
           comments = { italic = true },
           keywords = { italic = true },
           functions = {},
           variables = {},
-          sidebars = "dark",
-          floats = "dark",
+          sidebars = "transparent",
+          floats = "transparent",
         },
         sidebars = { "qf", "help", "neo-tree" },
         dim_inactive = false,
