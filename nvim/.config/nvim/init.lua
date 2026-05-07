9c389db3a0479790a79aae26c1ae3c13361800b4
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2026-05-07 15:57:53 +0800

    references

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -66,7 +66,10 @@ vim.keymap.set("n", "<leader>m", function()
   vim.cmd("delmarks " .. mark)
 end)
 
-
+vim.keymap.set('n', 'gD', '<CMD>Glance definitions<CR>')
+vim.keymap.set('n', 'gR', '<CMD>Glance references<CR>')
+vim.keymap.set('n', 'gY', '<CMD>Glance type_definitions<CR>')
+vim.keymap.set('n', 'gM', '<CMD>Glance implementations<CR>')
 
 
 
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -25,24 +25,27 @@
 -- }
 
 return {
-	{
-		"ibhagwan/fzf-lua",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			local fzf = require("fzf-lua")
+  {
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local fzf = require("fzf-lua")
 
-			fzf.setup({
-				winopts = {
-					border = "rounded",
-					preview = {
-						default = "bat",
-					},
-				},
-			})
+      fzf.setup({
+        winopts = {
+          border = "rounded",
+          preview = {
+            default = "bat",
+          },
+        },
+      })
 
-			vim.keymap.set("n", "<C-p>", fzf.files, {})
-			vim.keymap.set("n", "<leader>fg", fzf.live_grep, { desc = "fzf-lua live grep" })
-		end,
-	},
+      vim.keymap.set("n", "<C-p>", fzf.files, {})
+      vim.keymap.set("n", "<leader>fg", fzf.live_grep, { desc = "fzf-lua live grep" })
+    end,
+  },
+  {
+    'dnlhc/glance.nvim',
+    cmd = 'Glance'
+  }
 }
-
